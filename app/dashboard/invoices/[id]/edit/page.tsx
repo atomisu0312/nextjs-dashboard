d3b6dedee904a5c9604b772d93168cf24babca48
takahiro-maekawa <koma4024@gmail.com>
2024-03-25 16:38:06 +0900

    ch13 Handling Errors
    - 外部システムが関わる部分はtry catchで囲む
    - エラーページを配置する
    - 固有のエラー(Not Found)をスローさせる

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,7 +1,8 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
- 
+import { notFound } from 'next/navigation'; // not-found.tsxが表示されるように指示
+
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
   
@@ -9,6 +10,9 @@ export default async function Page({ params }: { params: { id: string } }) {
     fetchInvoiceById(id),
     fetchCustomers(),
   ]);
+  if (!invoice) {
+    notFound(); // notFound Errorをスローできる
+  }
   return (
     <main>
       <Breadcrumbs
